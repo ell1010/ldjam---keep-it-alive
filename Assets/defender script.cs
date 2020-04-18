8f70d4cd1615e64cdbcb5cabe003ef42be6badc4
ell_1010 <elliottbarrett25@gmail.com>
2020-04-18 13:39:42 +0100

    init

---
+++ Assets/defender script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class defenderscript : MonoBehaviour
+{
+	public int damage;
+	public int range;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
